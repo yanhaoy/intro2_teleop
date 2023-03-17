98607afcba1aced72005280ddccf911522232875
Yanhao Yang <yangyanh@oregonstate.edu>
2023-03-17 18:41:51 +0000

    Add teleop

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+import os  # nopep8
+import sys  # nopep8
+
+fpath = os.path.join(os.path.dirname(__file__), "ArmPi")  # nopep8
+sys.path.insert(0, fpath)  # nopep8
+
 import sys
 import zmq
 import time
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+
+AK = ArmIK()
 
 # Set the port
-port = "5556" # It should be fine to use the default
+port = "5556"  # It should be fine to use the default
 if len(sys.argv) > 1:
     port = sys.argv[1]
     int(port)
@@ -11,19 +25,41 @@ if len(sys.argv) > 1:
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.setsockopt(zmq.CONFLATE, 1) # Take only the last element
-socket.connect("tcp://192.168.149.39:%s" % port) # I hope this IP is constant, otherwise change it to your laptop's IP
+socket.setsockopt(zmq.CONFLATE, 1)  # Take only the last element
+socket.connect(
+    "tcp://192.168.149.39:%s" % port
+)  # I hope this IP is constant, otherwise change it to your laptop's IP
 
 # Set topic filter
-topicfilter = "control" # This should match the server's topic name
+topicfilter = "control"  # This should match the server's topic name
 socket.setsockopt(zmq.SUBSCRIBE, bytes(topicfilter, "utf-8"))
 
+x, y, z = 0, 10, 10
+
 while True:
-    time.sleep(0.5)
+    time.sleep(0.01)
     try:
-        res = socket.recv(flags=zmq.NOBLOCK) # Asynchronous communication using noblock
+        res = socket.recv(flags=zmq.NOBLOCK)  # Asynchronous communication using noblock
         topic, message = res.decode("utf-8").split()
         print("client receives: " + message + " at the topic: " + topic)
+
+        if message == "w":
+            x += 1
+        elif message == "a":
+            y -= 1
+        elif message == "s":
+            x -= 1
+        elif message == "d":
+            y += 1
+        elif message == "q":
+            z += 1
+        elif message == "e":
+            z -= 1
+        else:
+            continue
+
+        AK.setPitchRangeMoving((x, y, z), -30, -30, -90, 1500)
+
     except zmq.Again as e:
         # If not received, do something else
         print("No new message received yet")
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 import zmq
 import sys
 import time
+import pygame
+
+# initialising pygame
+pygame.init()
+
+# creating display
+display = pygame.display.set_mode((300, 300))
 
 # Set the port
-port = "5556" # It should be fine to use the default
+port = "5556"  # It should be fine to use the default
 if len(sys.argv) > 1:
     port = sys.argv[1]
     int(port)
@@ -14,18 +21,41 @@ socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:%s" % port)
 
 # Set topic
-topic = "control" # This should match the server's topic name
+topic = "control"  # This should match the server's topic name
 
 count = 0
 
 while True:
-    message = "command%d" % count
-    print("server sends: " + message + " at the topic: " + topic)
+    time.sleep(0.01)
 
-    # The full message is topic_space_message
-    fullmessage = bytes(topic + " " + message, "utf-8")
-    
-    socket.send(fullmessage)
+    # creating a loop to check events that
+    # are occurring
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-    count += 1
-    time.sleep(1)
+        # checking if keydown event happened or not
+        if event.type == pygame.KEYDOWN:
+            # checking if key "A" was pressed
+            if event.key == pygame.K_w:
+                message = "w"
+            elif event.key == pygame.K_a:
+                message = "a"
+            elif event.key == pygame.K_s:
+                message = "s"
+            elif event.key == pygame.K_d:
+                message = "d"
+            elif event.key == pygame.K_q:
+                message = "q"
+            elif event.key == pygame.K_e:
+                message = "e"
+            else:
+                continue
+
+            print("server sends: " + message + " at the topic: " + topic)
+
+            # The full message is topic_space_message
+            fullmessage = bytes(topic + " " + message, "utf-8")
+
+            socket.send(fullmessage)
